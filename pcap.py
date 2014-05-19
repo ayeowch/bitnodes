a210b6bd601c8d90995c92e70a72d4b85e33cedc
ayeowch <ayeowch@gmail.com>
2014-05-20 07:23:49 +1000

    Catch TypeError when getting start height from Redis

--- pcap.py
+++ pcap.py
@@ -60,7 +60,7 @@ def save_invs(timestamp, node, invs):
         logging.debug("[{}] {}:{}".format(timestamp, inv['type'], inv['hash']))
         key = "inv:{}:{}".format(inv['type'], inv['hash'])
         redis_pipe.zadd(key, timestamp, node)
-        redis_pipe.expire(key, 28800)  # Expires in 8 hours
+        redis_pipe.expire(key, 10800)  # Expires in 3 hours
     redis_pipe.execute()
 
 
