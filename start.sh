20d13c8ad00dd2f78aa1308a2959c016371a877b
ayeowch <500869+ayeowch@users.noreply.github.com>
2014-12-30 20:55:21 +1100

    Remove one slave instance of ping.py

--- start.sh
+++ start.sh
@@ -7,7 +7,6 @@ python -u ping.py ping.conf slave > ping.slave.1.out 2>&1 &
 python -u ping.py ping.conf slave > ping.slave.2.out 2>&1 &
 python -u ping.py ping.conf slave > ping.slave.3.out 2>&1 &
 python -u ping.py ping.conf slave > ping.slave.4.out 2>&1 &
-python -u ping.py ping.conf slave > ping.slave.5.out 2>&1 &
 
 python -u resolve.py resolve.conf > resolve.out 2>&1 &
 python -u export.py export.conf > export.out 2>&1 &
