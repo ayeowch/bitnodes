13df8380649b50cfddb90d5b98d0163b1e632658
ayeowch <500869+ayeowch@users.noreply.github.com>
2015-01-03 21:23:06 +1100

    Use fixed TTL for resolved data

--- start.sh
+++ start.sh
@@ -8,7 +8,9 @@ python -u ping.py ping.conf slave > ping.slave.2.out 2>&1 &
 python -u ping.py ping.conf slave > ping.slave.3.out 2>&1 &
 python -u ping.py ping.conf slave > ping.slave.4.out 2>&1 &
 
+# export GEVENT_RESOLVER=ares (Recommended only on Linux!)
 python -u resolve.py resolve.conf > resolve.out 2>&1 &
+
 python -u export.py export.conf > export.out 2>&1 &
 
 python -u seeder.py seeder.conf > seeder.out 2>&1 &
