809813e36afb646128f3a4ce8df41439c21a7fb7
ayeowch <500869+ayeowch@users.noreply.github.com>
2016-03-17 23:12:48 +1100

    Enable support to limit max. nodes per IPv6 network prefix

---
+++ utils.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# utils.py - Common helper methods.
+#
+# Copyright (c) Addy Yeow Chin Heng <ayeowch@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+Common helper methods.
+"""
+
+from ipaddress import ip_network
+
+
+def get_keys(redis_conn, pattern):
+    """
+    Returns Redis keys matching pattern by iterating the keys space.
+    """
+    keys = []
+    cursor = 0
+    while True:
+        (cursor, partial_keys) = redis_conn.scan(cursor, pattern)
+        keys.extend(partial_keys)
+        if cursor == 0:
+            break
+    return keys
+
+
+def ip_to_network(address, prefix):
+    """
+    Returns CIDR notation to represent the address and its prefix.
+    """
+    network = ip_network(unicode("{}/{}".format(address, prefix)),
+                         strict=False)
+    return "{}/{}".format(network.network_address, prefix)
