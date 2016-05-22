89a70295b53052bfd9e7541713b7713c32cb572d
ayeowch <ayeowch@gmail.com>
2016-05-22 10:30:56 +1000

    Do not set expire for existing key

--- pcap.py
+++ pcap.py
@@ -187,7 +187,8 @@ class Cache(object):
                         logging.debug("Skip: %s", key)
                         continue
                 self.redis_pipe.zadd(key, timestamp, self.node_hash(node))
-                self.redis_pipe.expire(key, SETTINGS['ttl'])
+                if not REDIS_CONN.exists(key):
+                    self.redis_pipe.expire(key, SETTINGS['ttl'])
             self.count += msg['count']
         elif msg['command'] == "pong":
             key = "ping:{}-{}:{}".format(node[0], node[1], msg['nonce'])
