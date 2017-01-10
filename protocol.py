4aa573a4fdc47225a603e4ced9c29834502cd5af
losh11 <loshan1212@gmail.com>
2017-01-10 03:44:46 +0000

    Bitcoin -> Litecoin

--- protocol.py
+++ protocol.py
@@ -25,7 +25,7 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 """
-Bitcoin protocol access for Bitnodes.
+Litecoin protocol access for Bitnodes.
 Reference: https://en.bitcoin.it/wiki/Protocol_specification
 
 -------------------------------------------------------------------------------
@@ -33,7 +33,7 @@ Reference: https://en.bitcoin.it/wiki/Protocol_specification
                            protocol version >= 70001
 -------------------------------------------------------------------------------
 [---MESSAGE---]
-[ 4] MAGIC_NUMBER               (\xF9\xBE\xB4\xD9)                  uint32_t
+[ 4] MAGIC_NUMBER               (\xFB\xC0\xB6\xDB)                  uint32_t
 [12] COMMAND                                                        char[12]
 [ 4] LENGTH                     <I (len(payload))                   uint32_t
 [ 4] CHECKSUM                   (sha256(sha256(payload))[:4])       uint32_t
@@ -155,15 +155,15 @@ TESTNET3 = "testnet3"
 MAINNET_MAGIC_NUMBER = "\xF9\xBE\xB4\xD9"
 TESTNET3_MAGIC_NUMBER = "\x0B\x11\x09\x07"
 
-MAINNET_DEFAULT_PORT = 8333
-TESTNET3_DEFAULT_PORT = 18333
+MAINNET_DEFAULT_PORT = 9333
+TESTNET3_DEFAULT_PORT = 19333
 
 MIN_PROTOCOL_VERSION = 70001
-PROTOCOL_VERSION = 70002
+PROTOCOL_VERSION = 70015
 FROM_SERVICES = 0
 TO_SERVICES = 1  # NODE_NETWORK
-USER_AGENT = "/bitnodes.21.co:0.1/"
-HEIGHT = 420000
+USER_AGENT = "/litenodes:1.0/"
+HEIGHT = 1130000
 RELAY = 0  # set to 1 to receive all txs
 
 SOCKET_BUFSIZE = 8192
@@ -933,7 +933,7 @@ class Connection(object):
 
 
 def main():
-    to_addr = ("136.243.139.96", MAINNET_DEFAULT_PORT)
+    to_addr = ("173.209.44.34", MAINNET_DEFAULT_PORT)
     to_services = TO_SERVICES
 
     handshake_msgs = []
