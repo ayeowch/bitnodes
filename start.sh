42200723814ee14afb20f454d197d5ea44dbdcff
ayeowch <500869+ayeowch@users.noreply.github.com>
2017-06-07 11:33:30 +1000

    Add slave instances to handle sybil spikes

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ python -u crawl.py crawl.mainnet.conf master > crawl.mainnet.master.out 2>&1 &
 python -u crawl.py crawl.mainnet.conf slave > crawl.mainnet.slave.1.out 2>&1 &
 python -u crawl.py crawl.mainnet.conf slave > crawl.mainnet.slave.2.out 2>&1 &
 python -u crawl.py crawl.mainnet.conf slave > crawl.mainnet.slave.3.out 2>&1 &
+python -u crawl.py crawl.mainnet.conf slave > crawl.mainnet.slave.4.out 2>&1 &
 
 python -u ping.py ping.conf master > ping.master.out 2>&1 &
 python -u ping.py ping.conf slave > ping.slave.1.out 2>&1 &
@@ -11,6 +12,10 @@ python -u ping.py ping.conf slave > ping.slave.2.out 2>&1 &
 python -u ping.py ping.conf slave > ping.slave.3.out 2>&1 &
 python -u ping.py ping.conf slave > ping.slave.4.out 2>&1 &
 python -u ping.py ping.conf slave > ping.slave.5.out 2>&1 &
+python -u ping.py ping.conf slave > ping.slave.6.out 2>&1 &
+python -u ping.py ping.conf slave > ping.slave.7.out 2>&1 &
+python -u ping.py ping.conf slave > ping.slave.8.out 2>&1 &
+python -u ping.py ping.conf slave > ping.slave.9.out 2>&1 &
 
 python -u resolve.py resolve.conf > resolve.out 2>&1 &
 
@@ -20,6 +25,7 @@ python -u seeder.py seeder.conf > seeder.out 2>&1 &
 
 python -u pcap.py pcap.conf > pcap.1.out 2>&1 &
 python -u pcap.py pcap.conf > pcap.2.out 2>&1 &
+python -u pcap.py pcap.conf > pcap.3.out 2>&1 &
 
 # --- testnet3 ---
 python -u crawl.py crawl.testnet3.conf master > crawl.testnet3.master.out 2>&1 &
