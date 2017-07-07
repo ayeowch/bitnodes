6d3c81114ade6426eea4626d6ad25c4121327304
Ryan Romanchuk <rromanchuk@gmail.com>
2017-07-06 17:26:51 -0700

    append

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # --- mainnet ---
 rm -rf data/*
-python -u crawl.py crawl.conf.default master > log.out 2>&1 &
-python -u crawl.py crawl.conf.default slave > log.out 2>&1 &
+python -u crawl.py crawl.conf.default master >> log.out 2>&1 &
+python -u crawl.py crawl.conf.default slave >> log.out 2>&1 &
 # python -u crawl.py crawl.conf.default slave > crawl.mainnet.slave.2.out 2>&1 &
 # python -u crawl.py crawl.conf.default slave > crawl.mainnet.slave.3.out 2>&1 &
 # python -u crawl.py crawl.conf.default slave > crawl.mainnet.slave.4.out 2>&1 &
 
-python -u ping.py ping.conf.default master > log.out 2>&1 &
-python -u ping.py ping.conf.default slave > log.out 2>&1 &
-python -u ping.py ping.conf.default slave > log.out 2>&1 &
+python -u ping.py ping.conf.default master >> log.out 2>&1 &
+python -u ping.py ping.conf.default slave >> log.out 2>&1 &
+python -u ping.py ping.conf.default slave >> log.out 2>&1 &
 # python -u ping.py ping.conf slave > ping.slave.3.out 2>&1 &
 # python -u ping.py ping.conf slave > ping.slave.4.out 2>&1 &
 # python -u ping.py ping.conf slave > ping.slave.5.out 2>&1 &
@@ -18,15 +18,15 @@ python -u ping.py ping.conf.default slave > log.out 2>&1 &
 # python -u ping.py ping.conf slave > ping.slave.8.out 2>&1 &
 # python -u ping.py ping.conf slave > ping.slave.9.out 2>&1 &
 
-python -u resolve.py resolve.conf.default > log.out 2>&1 &
+python -u resolve.py resolve.conf.default >> log.out 2>&1 &
 
-python -u export.py export.conf.default > log.out 2>&1 &
+python -u export.py export.conf.default >> log.out 2>&1 &
 
-python -u seeder.py seeder.conf.default > log.out 2>&1 &
+python -u seeder.py seeder.conf.default >> log.out 2>&1 &
 
-python -u pcap.py pcap.conf.default > log.out 2>&1 &
-python -u pcap.py pcap.conf.default > log.out 2>&1 &
-python -u pcap.py pcap.conf.default > log.out 2>&1 &
+python -u pcap.py pcap.conf.default >> log.out 2>&1 &
+python -u pcap.py pcap.conf.default >> log.out 2>&1 &
+python -u pcap.py pcap.conf.default >> log.out 2>&1 &
 
 # --- testnet3 ---
 #python -u crawl.py crawl.testnet3.conf master > crawl.testnet3.master.out 2>&1 &
