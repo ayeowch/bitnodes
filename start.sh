9c1e4a9222ff02f4508358c1ffc37e72a3db4641
Ryan Romanchuk <rromanchuk@gmail.com>
2017-07-06 17:03:17 -0700

    simplify

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # --- mainnet ---
-python -u crawl.py crawl.conf.default master > crawl.mainnet.master.out 2>&1 &
-python -u crawl.py crawl.conf.default slave > crawl.mainnet.slave.1.out 2>&1 &
+rm -rf data/*
+python -u crawl.py crawl.conf.default master > log.out 2>&1 &
+python -u crawl.py crawl.conf.default slave > log.out 2>&1 &
 # python -u crawl.py crawl.conf.default slave > crawl.mainnet.slave.2.out 2>&1 &
 # python -u crawl.py crawl.conf.default slave > crawl.mainnet.slave.3.out 2>&1 &
 # python -u crawl.py crawl.conf.default slave > crawl.mainnet.slave.4.out 2>&1 &
 
-python -u ping.py ping.conf.default master > ping.master.out 2>&1 &
-python -u ping.py ping.conf.default slave > ping.slave.1.out 2>&1 &
-python -u ping.py ping.conf.default slave > ping.slave.2.out 2>&1 &
+python -u ping.py ping.conf.default master > log.out 2>&1 &
+python -u ping.py ping.conf.default slave > log.out 2>&1 &
+python -u ping.py ping.conf.default slave > log.out 2>&1 &
 # python -u ping.py ping.conf slave > ping.slave.3.out 2>&1 &
 # python -u ping.py ping.conf slave > ping.slave.4.out 2>&1 &
 # python -u ping.py ping.conf slave > ping.slave.5.out 2>&1 &
@@ -17,19 +18,19 @@ python -u ping.py ping.conf.default slave > ping.slave.2.out 2>&1 &
 # python -u ping.py ping.conf slave > ping.slave.8.out 2>&1 &
 # python -u ping.py ping.conf slave > ping.slave.9.out 2>&1 &
 
-python -u resolve.py resolve.conf.default > resolve.out 2>&1 &
+python -u resolve.py resolve.conf.default > log.out 2>&1 &
 
-python -u export.py export.conf.default > export.out 2>&1 &
+python -u export.py export.conf.default > log.out 2>&1 &
 
-python -u seeder.py seeder.conf.default > seeder.out 2>&1 &
+python -u seeder.py seeder.conf.default > log.out 2>&1 &
 
-python -u pcap.py pcap.conf.default > pcap.1.out 2>&1 &
-python -u pcap.py pcap.conf.default > pcap.2.out 2>&1 &
-python -u pcap.py pcap.conf.default > pcap.3.out 2>&1 &
+python -u pcap.py pcap.conf.default > log.out 2>&1 &
+python -u pcap.py pcap.conf.default > log.out 2>&1 &
+python -u pcap.py pcap.conf.default > log.out 2>&1 &
 
 # --- testnet3 ---
 #python -u crawl.py crawl.testnet3.conf master > crawl.testnet3.master.out 2>&1 &
 
-cd data/pcap
-sudo rm *.pcap; sudo tcpdump -i eth0 -w %s.eth0.pcap -v -n -G 2 -B 65536 -Z ubuntu 'tcp and not src host 54.208.195.206' > eth0 2>&1 &
-sudo tcpdump -i lo -w %s.lo.pcap -v -n -G 2 -B 65536 -Z ubuntu 'tcp and port 9050' > lo 2>&1 &
+# cd data/pcap
+# sudo rm *.pcap; sudo tcpdump -i eth0 -w %s.eth0.pcap -v -n -G 2 -B 65536 -Z ubuntu 'tcp and not src host 54.208.195.206' > eth0 2>&1 &
+# sudo tcpdump -i lo -w %s.lo.pcap -v -n -G 2 -B 65536 -Z ubuntu 'tcp and port 9050' > lo 2>&1 &
