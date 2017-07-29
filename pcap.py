e203ff043cd8c2f762e32c29cf88844a1b2b37d8
Ryan Romanchuk <rromanchuk@gmail.com>
2017-07-29 10:12:18 -0700

    add debugging better insight on success vs failure

--- pcap.py
+++ pcap.py
@@ -135,6 +135,7 @@ class Cache(object):
                     node = src
                     if src == SETTINGS['tor_proxy']:
                         node = dst
+                    logging.info("Sucess %s: %s", node, msg)
                     self.cache_message(node, self.stream.timestamp, msg)
         self.redis_pipe.execute()
         self.cache_rtt()
