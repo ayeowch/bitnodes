09a4cde092788dc8273af68540bc6cb740de44c6
Ryan Romanchuk <rromanchuk@gmail.com>
2017-08-02 17:36:09 -0700

    rest the the entire system

---
+++ reset.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo systemctl stop crawl_slave
+sudo systemctl stop crawl
+
+
+sudo systemctl stop ping_slave
+sudo systemctl stop ping
+
+sudo systemctl stop pcap
+sudo systemctl stop resolve
+sudo systemctl stop seeder
+sudo systemctl stop export
+
+redis-cli flushdb
+rm data/crawl/mainnet/*.json
+rm data/export/*.json
+rm data/pcap/*.json
+
+rm *.log
